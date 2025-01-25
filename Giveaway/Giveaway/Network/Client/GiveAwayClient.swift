d68f92c659baf1ac4b943629d13753d536045f49
Bakr Mohamed <>
2025-01-25 13:03:17 +0200

    Add Network Requests and Client

---
+++ Giveaway/Giveaway/Network/Client/GiveAwayClient.swift
@@ -0,0 +1,27 @@
+//
+//  GiveAwayClient.swift
+//  Giveaway
+//
+//  Created by Bakr mohamed on 25/01/2025.
+//
+
+protocol GiveAwayClientProtocol {
+    func getGiveAways(_ requestModel: GiveAwayRequestModel?) async -> Result<[GiveAwayItem], APIError>
+    func getGiveAwayDetail(_ id: Int) async -> Result<GiveAwayItem, APIError>
+}
+
+
+struct GiveAwayClient: GiveAwayClientProtocol {
+    
+    func getGiveAways(_ requestModel: GiveAwayRequestModel? = nil) async -> Result<[GiveAwayItem], APIError> {
+        if let requestModel = requestModel {
+            return await GiveAwayRequests.GetFilteredGiveAways(requestModel: requestModel).performResult()
+        } else {
+            return await GiveAwayRequests.GetAllGiveAways().performResult()
+        }
+    }
+    
+    func getGiveAwayDetail(_ id: Int) async -> Result<GiveAwayItem, APIError> {
+        return await GiveAwayRequests.GetdGiveAwayDetails(itemId: id).performResult()
+    }
+}
